80e5a126ffcf20ad5b5fc7f76ae9a6ea9e79ef21
Jan Kamnikar <82883128+JanKamnikar@users.noreply.github.com>
2023-09-07 00:04:20 +0200

    Podatki o igralcih

--- film-podatki.py
+++ film-podatki.py
@@ -4,17 +4,42 @@ import os
 import re
 import random
 
+def poisci_vec(reg1, reg2, vsebina, locilo=','):
+    genres = re.findall(reg1, vsebina, re.DOTALL)
+    genres = genres[0].split(locilo)
+    genres_seznam = []
+    for genre in genres:
+        try:
+            genres_seznam.append(re.findall(reg2, genre, re.DOTALL)[0])
+        except:
+            pass
+    return genres_seznam
+
+def print_diagnostic(msg):
+    print('='*20)
+    print(msg)
+    print('='*20)
+    1/0
+
+
 seznam_filmov = set()
-html_datoteke = './zajeti-podatki/seznami-filmov/'
+html_datoteke = './zajeti-podatki/filmi/'
 # list all files in the directory and read them
 for filename in os.listdir(html_datoteke):
     vsebina = orodja.vsebina_datoteke(html_datoteke + filename)
-
-    naslov = re.findall(r'<h2 class="movie-title">.*?<\/h2>', vsebina, re.DOTALL)
-    leto = re.findall(r'<span class="release-year">.*?<\/span>', vsebina, re.DOTALL)
-    reziser = re.findall(r'<span class="director">.*?<\/span>', vsebina, re.DOTALL)
-    podatki = re.findall(r'<div class="details movie">.*?<\/div>', vsebina, re.DOTALL)
+    print( filename)
+    naslov = re.findall(r'<h2 class="movie-title">\s*(.*?)\s*<span', vsebina, re.DOTALL)
+    leto = re.findall(r'<span class="release-year">\((.*?)\)<\/span>', vsebina, re.DOTALL)
+    reziserji = poisci_vec(r'<h3 class="movie-director".*?<\/h3>', r'<a.*?>(.*?)<\/a>', vsebina, locilo='/')
+    print_diagnostic(reziserji)
+    reziser = re.findall(r'<h3 class="movie-director".*?<a.*?>(.*?)<\/a>', vsebina, re.DOTALL)
+    details = re.findall(r'<div class="details">.*?<\/div>', vsebina, re.DOTALL)
+    
+    zanri = poisci_vec(r'<span class="header-movie-genres".*?<\/span>', r'<a.*?>(.*?)<\/a>', vsebina)
+    drzave = poisci_vec(r'<span.*?Countries.*?(<span.*<\/span>)<span>.*?MPAA', r'<a.*?>(.*?)<\/a>', vsebina)
+    run_time = re.findall(r'<span.*?Run Time -.*?(\d*?) min.*?<\/span>', vsebina, re.DOTALL)
     ocene = re.findall(r'<ul class="ratings">.*?<\/ul>', vsebina, re.DOTALL)
+
     karakteristike = re.findall(r'<section class="characteristics">.*?<\/section>', vsebina, re.DOTALL)
     osnovni_podatki = re.findall(r'<section class="basic-info">.*?<\/section>', vsebina, re.DOTALL)
 
@@ -27,7 +52,6 @@ for filename in os.listdir(html_datoteke):
 
     print(reziser[0])
 
-    print('-'*20)
     1/0
 
     kartice = re.findall(vzorec, vsebina, re.DOTALL)
