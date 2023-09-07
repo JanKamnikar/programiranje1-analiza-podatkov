f995b192b6ac42117583390fe461b244bb8a5477
Jan Kamnikar <82883128+JanKamnikar@users.noreply.github.com>
2023-09-07 02:58:01 +0200

    obdelani podatki

--- film-podatki.py
+++ film-podatki.py
@@ -3,9 +3,12 @@ import time
 import os 
 import re
 import random
+import pandas as pd
 
 def poisci_vec(reg1, reg2, vsebina, locilo=','):
     genres = re.findall(reg1, vsebina, re.DOTALL)
+    if not genres:
+        return []
     genres = genres[0].split(locilo)
     genres_seznam = []
     for genre in genres:
@@ -21,51 +24,97 @@ def print_diagnostic(msg):
     print('='*20)
     1/0
 
+mn_ljudi = set()
+mn_zanrov = set()
+mn_drzav = set()
+mn_tem = set()
+mn_oznak = set()
+
+podatki = []
+
 
 seznam_filmov = set()
 html_datoteke = './zajeti-podatki/filmi/'
 # list all files in the directory and read them
-for filename in os.listdir(html_datoteke):
-    vsebina = orodja.vsebina_datoteke(html_datoteke + filename)
-    print( filename)
-    naslov = re.findall(r'<h2 class="movie-title">\s*(.*?)\s*<span', vsebina, re.DOTALL)
-    leto = re.findall(r'<span class="release-year">\((.*?)\)<\/span>', vsebina, re.DOTALL)
-    reziserji = poisci_vec(r'<h3 class="movie-director".*?<\/h3>', r'<a.*?>(.*?)<\/a>', vsebina, locilo=' / ')
-    opis = re.findall(r'<div class="text">(.*?)<\/div>', vsebina, re.DOTALL)[0].strip()
-    zanri = poisci_vec(r'<span class="header-movie-genres".*?<\/span>', r'<a.*?>(.*?)<\/a>', vsebina)
-    drzave = poisci_vec(r'<span>.*?Countries(.*?)MPAA', r'<span.*?>(.*?)<\/span>', vsebina)
-    run_time = int(re.findall(r'<span.*?Run Time -.*?(\d*?) min.*?<\/span>', vsebina, re.DOTALL)[0].strip())
-    ocena_kritikov = int(re.findall(r'<div class="allmovie-rating.*?>(.*?)<\/div>', vsebina, re.DOTALL)[0].strip())
-    teme = poisci_vec(r'<div class="themes.*?<div class="charactList.*?<\/div>', r'<a.*?>(.*?)<\/a>', vsebina, '|')
-    oznaka = re.findall(r'<div class="mpaa">.*?<div>(.*?)<\/div>', vsebina, re.DOTALL)
-    flags = poisci_vec(r'<div class="flags">.*?<div>(.*?)<\/div>', r'<span.*?>(.*?)<', vsebina, '/')
-    legacy_id = re.findall(r'<div class="legacy-id">.*?<div>(.*?)<\/div>', vsebina, re.DOTALL)
-    html_datoteke_igralci = './zajeti-podatki/filmi-cast/'
-    vsebina_igralci = orodja.vsebina_datoteke(html_datoteke_igralci + filename)
+for i,filename in enumerate(os.listdir(html_datoteke)):
+    print(i+1, 'od', '~1017:', filename)
+    try:
 
-    igralci = poisci_vec(r'<div class="description-box">.*?<\/section>', r'<a.*?tooltip.*?>(.*?)<\/a>', vsebina_igralci, 'cast_name')
-    print_diagnostic(igralci)
-    raw_podatki_o_filmu = {
-        'naslov': naslov[0],
-        'leto': leto[0],
-    }
+        vsebina = orodja.vsebina_datoteke(html_datoteke + filename)
+        naslov = re.findall(r'<h2 class="movie-title">\s*(.*?)\s*<span', vsebina, re.DOTALL)
+        leto = re.findall(r'<span class="release-year">\((.*?)\)<\/span>', vsebina, re.DOTALL)
+        oznaka = re.findall(r'<div class="mpaa">.*?<div>(.*?)<\/div>', vsebina, re.DOTALL)
+        if oznaka:
+            oznaka = oznaka[0].strip()
+        opis = re.findall(r'<div class="text">(.*?)<\/div>', vsebina, re.DOTALL)
+        if opis:
+            opis = opis[0].strip()
+        run_time = re.findall(r'<span.*?Run Time -.*?(\d*?) min.*?<\/span>', vsebina, re.DOTALL)
+        if run_time:
+            run_time = int(run_time[0].strip())
+        ocena_kritikov = re.findall(r'<div class="allmovie-rating.*?>(.*?)<\/div>', vsebina, re.DOTALL)
+        if ocena_kritikov:
+            ocena_kritikov = int(ocena_kritikov[0].strip())
 
-    # Reziser 
+        html_datoteke_igralci = './zajeti-podatki/filmi-cast/'
+        vsebina_igralci = orodja.vsebina_datoteke(html_datoteke_igralci + filename)
 
-    print(reziser[0])
+        igralci = poisci_vec(r'<div class="description-box">(.*?)<\/section>', r'<div class="cast_name.*?<a.*?tooltip.*?>(.*?)<\/a>', vsebina_igralci, 'cast_name')
+        reziserji = poisci_vec(r'<h3 class="movie-director"(.*?)<\/h3>', r'<a.*?>(.*?)<\/a>', vsebina, locilo=' / ')
+        zanri = poisci_vec(r'<span class="header-movie-genres".*?<\/span>', r'<a.*?>(.*?)<\/a>', vsebina)
+        drzave = poisci_vec(r'<span>.*?Countries(.*?)MPAA', r'<span.*?>(.*?)<\/span>', vsebina)
+        teme = poisci_vec(r'<div class="themes.*?<div class="charactList.*?<\/div>', r'<a.*?>(.*?)<\/a>', vsebina, '|')
+        flags = poisci_vec(r'<div class="flags">.*?<div>(.*?)<\/div>', r'<span.*?>(.*?)<', vsebina, '/')
 
-    1/0
+        mn_ljudi.update(igralci)
+        mn_ljudi.update(reziserji)
+        mn_zanrov.update(zanri)
+        mn_drzav.update(drzave)
+        mn_tem.update(teme)
+        mn_oznak.update(flags)
 
-    kartice = re.findall(vzorec, vsebina, re.DOTALL)
-    for div in kartice:
-        vzorec2 = r'<a class="img-link" href="(.*?)"'
-        link = re.findall(vzorec2, div, re.DOTALL)
-        seznam_filmov.add(link[0])
+        raw_podatki_o_filmu = {
+            'naslov': naslov[0],
+            'leto': int(leto[0]),
+            'opis': opis,
+            'trajanje': run_time,
+            'ocena_kritikov': ocena_kritikov,
+            'oznaka': oznaka,
 
+            'igralci': igralci,
+            'reziserji': reziserji,
+            'zanri': zanri,
+            'drzave': drzave,
+            'teme': teme,
+            'oznake': flags
+        }
+        podatki.append(raw_podatki_o_filmu)
+    except Exception as e:
+        print('Napaka pri branju podatkov o filmu:', i+1)
+        raise e
 
+seznam_ljudi = list(mn_ljudi)
+seznam_zanrov = list(mn_zanrov)
+seznam_drzav = list(mn_drzav)
+seznam_tem = list(mn_tem)
+seznam_oznak = list(mn_oznak)
 
-for film in seznam_filmov:
-    ime_datoteke = 'zajeti-podatki/filmi/stran' + film.replace('/', '-') + '.html'
-    spletna_stran = f'https://www.allmovie.com{film}'
-    orodja.shrani_spletno_stran(spletna_stran, ime_datoteke)
-    time.sleep(1 + 4 * random.random())
+orodja.zapisi_json(podatki, './obdelani-podatki/filmi.json')
+
+for i,podatek in enumerate(podatki):
+    print(i+1, 'od', len(podatki))
+    podatek['id'] = i
+    podatek['igralci'] = ','.join([str(seznam_ljudi.index(igralec)) for igralec in podatek['igralci']])
+    podatek['reziserji'] = ','.join([str(seznam_ljudi.index(reziser)) for reziser in podatek['reziserji']])
+    podatek['zanri'] = ','.join([str(seznam_zanrov.index(zanr)) for zanr in podatek['zanri']])
+    podatek['drzave'] = ','.join([str(seznam_drzav.index(drzava)) for drzava in podatek['drzave']])
+    podatek['teme'] = ','.join([str(seznam_tem.index(tema)) for tema in podatek['teme']])
+    podatek['oznake'] = ','.join([str(seznam_oznak.index(zastava)) for zastava in podatek['oznake']])
+
+imena = ['filmi', 'ljudje', 'zanri', 'drzave', 'teme', 'oznake']
+for i,seznam in enumerate([podatki, seznam_ljudi, seznam_zanrov, seznam_drzav, seznam_tem, seznam_oznak]):
+    df = pd.DataFrame(seznam)
+    df.index.name = 'id'
+    if i != 0:
+        df.columns = ['podatek']
+    df.to_csv('./obdelani-podatki/' + imena[i] + '.csv')
